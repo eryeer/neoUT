be8676097363bb7234c0748eb9536ed93f1ff943
Charis <eryeer@163.com>
2019-07-23 17:30:08 +0800

    test plugin

---
+++ neo.UnitTests/Plugins/TestPolicyPlugin.cs
@@ -0,0 +1,43 @@
+﻿using Neo.Network.P2P.Payloads;
+using Neo.Plugins;
+using System.Collections.Generic;
+
+namespace Neo.UnitTests.Plugins
+{
+    class TestPolicyPlugin : Plugin, IPolicyPlugin
+    {
+        public TestPolicyPlugin() : base() { }
+
+        public override void Configure() { }
+
+        public IEnumerable<Transaction> FilterForBlock(IEnumerable<Transaction> transactions)
+        {
+            return transactions;
+        }
+
+        public bool FilterForMemoryPool(Transaction tx)
+        {
+            return false;
+        }
+
+        public bool TestOnMessage(object message)
+        {
+            return OnMessage(message);
+        }
+
+        public static bool TestResumeNodeStartup()
+        {
+            return ResumeNodeStartup();
+        }
+
+        public static void TestSuspendNodeStartup()
+        {
+            SuspendNodeStartup();
+        }
+
+        public static void TestLoadPlugins(NeoSystem system)
+        {
+            LoadPlugins(system);
+        }
+    }
+}
