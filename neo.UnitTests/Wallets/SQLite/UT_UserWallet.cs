d37188347a42a00df0e6914e13a6a7562c42146b
Charis <eryeer@163.com>
2019-07-24 15:09:53 +0800

    test UserWallet

---
+++ neo.UnitTests/Wallets/SQLite/UT_UserWallet.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Wallets.SQLite;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Security;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace Neo.UnitTests.Wallets.SQLite
+{
+    [TestClass]
+    public class UT_UserWallet
+    {
+        private string path;
+        private UserWallet wallet;
+        public static string GetRandomPath()
+        {
+            string threadName = Thread.CurrentThread.ManagedThreadId.ToString();
+            return Path.GetFullPath(string.Format("Wallet_{0}", new Random().Next(1, 1000000).ToString("X8")) + threadName);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            path = GetRandomPath();
+            wallet = UserWallet.Create(path, "123456");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            TestUtils.DeleteFile(path);
+        }
+
+        [TestMethod]
+        public void TestGetName()
+        {
+            wallet.Name.Should().Be(Path.GetFileNameWithoutExtension(path));
+        }
+
+        [TestMethod]
+        public void TestGetVersion()
+        {
+            Action action = () => wallet.Version.ToString();
+            action.ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void TestCreateAndOpenSecureString() {
+            string myPath = GetRandomPath();
+            var ss = new SecureString();
+            ss.AppendChar('a');
+            ss.AppendChar('b');
+            ss.AppendChar('c');
+
+            var w1 =  UserWallet.Create(myPath,ss);
+            w1.Should().NotBeNull();
+
+            var w2 = UserWallet.Open(myPath, ss);
+            w2.Should().NotBeNull();
+
+            ss.AppendChar('d');
+            Action action = () =>UserWallet.Open(myPath, ss);
+            action.ShouldThrow<CryptographicException>();
+
+            TestUtils.DeleteFile(myPath);
+        }
+    }
+}
