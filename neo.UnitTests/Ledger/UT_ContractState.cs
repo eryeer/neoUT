5b1e7ad037c3e258047c7f2f32753f4fe0735436
YANG <yangxiaoyun@neo.org>
2019-07-25 18:30:15 +0800

    add UT_ContractState

---
+++ neo.UnitTests/Ledger/UT_ContractState.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.IO.Json;
+using Neo.Ledger;
+using Neo.SmartContract;
+using Neo.SmartContract.Manifest;
+
+namespace Neo.UnitTests.Ledger
+{
+    [TestClass]
+    public class UT_ContractState
+    {
+        ContractState contract;
+        byte[] script = { 0x01 };
+        ContractManifest manifest;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            manifest = ContractManifest.CreateDefault(UInt160.Parse("0xa400ff00ff00ff00ff00ff00ff00ff00ff00ff01"));
+            contract = new ContractState
+            {
+                Script = script,
+                Manifest = manifest
+            };
+        }
+        [TestMethod]
+        public void TestConstructor()
+        {  
+            contract.Script.Should().BeEquivalentTo(script);
+            contract.Manifest.Should().Be(manifest);
+        }
+
+        [TestMethod]
+        public void TestGetHasStorage()
+        {
+            contract.HasStorage.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TestGetPayable()
+        {
+            contract.Payable.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TestGetScriptHash()
+        {
+            // _scriptHash == null
+            contract.ScriptHash.Should().Be(script.ToScriptHash());
+            // _scriptHash != null
+            contract.ScriptHash.Should().Be(script.ToScriptHash());
+        }
+
+        [TestMethod]
+        public void TestToJson()
+        {
+            JObject json = contract.ToJson();
+            json["hash"].ToString().Should().Be("\"0x820944cfdc70976602d71b0091445eedbc661bc5\"");
+            json["script"].ToString().Should().Be("\"01\"");
+            json["manifest"].ToString().Should().Be(manifest.ToJson().ToString());
+        }
+    }
+}
