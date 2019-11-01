0115457233c38be871838e90634f3aa1123d7cf4
Charis <eryeer@163.com>
2019-11-01 15:54:47 +0800

    commit

--- neo/Network/P2P/ProtocolHandler.cs
+++ neo/Network/P2P/ProtocolHandler.cs
@@ -104,6 +104,7 @@ namespace Neo.Network.P2P
             //phase1
             if (!(message is Message msg))
             {
+                Console.WriteLine(message.GetType());
                 countReturnedPhase1++;
                 return;
             }
